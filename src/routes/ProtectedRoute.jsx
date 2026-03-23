cc1f800b014e340ac0eadca6da9666972bad8149
Munyeshya <obedbenny54@gmail.com>
2026-03-23 16:40:27 +0200

    Make protected route page

--- src/routes/ProtectedRoute.jsx
+++ src/routes/ProtectedRoute.jsx
@@ -1,12 +1,17 @@
-import { Navigate, Outlet } from 'react-router-dom'
+import { Navigate, Outlet, useLocation } from 'react-router-dom'
 import { getToken, getUser } from '../utils/storage'
 
 function ProtectedRoute({ allowedRoles = [] }) {
+  const location = useLocation()
   const token = getToken()
   const user = getUser()
 
   if (!token) {
-    return <Navigate to="/login" replace />
+    return <Navigate to="/login" replace state={{ from: location }} />
+  }
+
+  if (!user) {
+    return <Navigate to="/login" replace state={{ from: location }} />
   }
 
   if (allowedRoles.length > 0 && !allowedRoles.includes(user?.role)) {
