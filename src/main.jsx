72fba4b32747f96f3102526192c7e65934b45d79
Munyeshya <obedbenny54@gmail.com>
2026-04-04 01:42:09 +0200

    Remove theme provider wiring

--- src/main.jsx
+++ src/main.jsx
@@ -2,15 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import { ToastProvider } from './components/feedback/ToastProvider'
-import { ThemeProvider } from './theme/ThemeProvider'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ThemeProvider>
-      <ToastProvider>
-        <App />
-      </ToastProvider>
-    </ThemeProvider>
+    <ToastProvider>
+      <App />
+    </ToastProvider>
   </React.StrictMode>
 )
